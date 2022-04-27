8e5783e6b32d3fbf1e319ccc6ad73dcaba9ea4e3
druzm <dr540@ast.cam.ac.uk>
2022-04-27 14:27:23 +0100

    export custom warning

--- gaiaxpy/core/__init__.py
+++ gaiaxpy/core/__init__.py
@@ -2,7 +2,7 @@ from . import generic_functions
 from .generic_functions import array_to_symmetric_matrix, _extract_systems_from_data, \
                                _get_spectra_type, _get_system_label, _progress_tracker, \
                                _validate_arguments, _validate_pwl_sampling, \
-                               _validate_wl_sampling
+                               _validate_wl_sampling, _warning
 
 from . import config
 from .config import _load_xpmerge_from_csv, _load_offset_from_csv, \

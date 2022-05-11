8ce286cb7725f3fca4f316620acb192bf2e7be0e
druzm <dr540@ast.cam.ac.uk>
2022-05-11 21:27:27 +0100

    remove unused files

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="GaiaXPy",
-    version="0.1.0",
+    version="0.1.2a",
     author="Francesca De Angeli, Paolo Montegriffo, Lovro Palaversa, Daniela Ruz-Mieres",
     author_email="fda@ast.cam.ac.uk",
     maintainer="Daniela Ruz-Mieres",
@@ -29,14 +29,14 @@ setup(
     description="Utilities to handle BP/RP (XP) Gaia low-resolution spectra as delivered via the archive",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="http://gitlab.com/pyxp-developers",
+    url="https://ioadpc.ast.cam.ac.uk/internalpydocs/",
     python_requires='>=3.6',
     packages=find_packages(),
     install_requires=required_packages,
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3-Clause License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
     ]
 )

fcb394bad016acd6e3ab3ee8ec3e937969cdbaa8
druzm <dr540@ast.cam.ac.uk>
2022-05-12 11:01:32 +0100

    add array columns

--- gaiaxpy/file_parser/parse_external.py
+++ gaiaxpy/file_parser/parse_external.py
@@ -29,6 +29,20 @@ class ExternalParser(GenericParser):
         """
         return super()._parse_csv(csv_file, array_columns)
 
+    def _parse_fits(self, fits_file, array_columns=array_columns):
+        """
+        Parse the input FITS file and store the result in a pandas DataFrame if it
+        contains externally calibrated sampled spectra.
+
+        Args:
+            fits_file (str): Path to an FITS file.
+            array_columns (list): List of columns in the file that contain arrays as strings.
+
+        Returns:
+            DataFrame: Pandas DataFrame representing the FITS file.
+        """
+        return super()._parse_fits(fits_file, array_columns=array_columns)
+
     def _parse_xml(self, xml_file, array_columns=array_columns):
         """
         Parse the input XML file and store the result in a pandas DataFrame if it
@@ -41,4 +55,4 @@ class ExternalParser(GenericParser):
         Returns:
             DataFrame: Pandas DataFrame representing the XML file.
         """
-        return super()._parse_xml(xml_file, array_columns)
+        return super()._parse_xml(xml_file, array_columns=array_columns)

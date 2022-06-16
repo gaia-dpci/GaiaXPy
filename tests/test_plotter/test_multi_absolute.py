159469c4e83c171272a7bc150eae08711a5162cf
druzm <dr540@ast.cam.ac.uk>
2022-06-16 14:30:18 +0100

    update tests

--- tests/test_plotter/test_multi_absolute.py
+++ tests/test_plotter/test_multi_absolute.py
@@ -13,22 +13,22 @@ class TestMultiAbsolute(unittest.TestCase):
     def test_multi_absolute_plotter(self):
         mean_spectra, sampling = calibrate(mean_spectrum_avro, save_file=False)
         plot_spectra(mean_spectra, sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_with_legend', format='pdf', legend=True)
+                     output_file='multi_abs_with_legend', format='pdf', legend=True)
         plot_spectra(mean_spectra, sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_without_legend', format='pdf', legend=False)
+                     output_file='multi_abs_without_legend', format='pdf', legend=False)
 
     def test_multi_absolute_single_element(self):
         mean_spectra, sampling = calibrate(mean_spectrum_csv, save_file=False)
         plot_spectra(mean_spectra.iloc[[0]], sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_with_legend_single', format='pdf', legend=True)
+                     output_file='multi_abs_with_legend_single', format='pdf', legend=True)
         plot_spectra(mean_spectra.iloc[[1]], sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_without_legend_single', format='pdf', legend=False)
+                     output_file='multi_abs_without_legend_single', format='pdf', legend=False)
 
     def test_multi_absolute_range_elements(self):
         mean_spectra, sampling = calibrate(mean_spectrum_csv, save_file=False)
         index_list = list(range(2))
         aux_mean_spectra = mean_spectra.iloc[index_list]
         plot_spectra(aux_mean_spectra, sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_with_legend_range', format='pdf', legend=True)
+                     output_file='multi_abs_with_legend_range', format='pdf', legend=True)
         plot_spectra(aux_mean_spectra, sampling=sampling, multi=True, show_plot=False, output_path=None,
-                     file_name='multi_abs_without_legend_range', format='pdf', legend=False)
+                     output_file='multi_abs_without_legend_range', format='pdf', legend=False)

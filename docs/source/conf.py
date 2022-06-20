78bff6c873f23d5a7f92ee7147bcdfe4df83f45d
druzm <dr540@ast.cam.ac.uk>
2022-06-20 14:17:00 +0100

    do not add css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ def edit_default_value(app, what, name, obj, options, signature, return_annotati
 
 
 def setup(app):
-    app.add_css_file('css/custom.css')
+    #app.add_css_file('css/custom.css')
     app.connect("autodoc-process-signature", edit_default_value)
 
 

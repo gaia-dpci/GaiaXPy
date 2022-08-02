f7e82ceac9e7cd92a164ea1190d26c5dcac1f2bb
druzm <dr540@ast.cam.ac.uk>
2022-08-02 15:29:38 +0100

    get inv cov matrix, file, many lines

--- gaiaxpy/cholesky/cholesky.py
+++ gaiaxpy/cholesky/cholesky.py
@@ -3,6 +3,9 @@ from scipy.linalg import cholesky, solve_triangular
 from gaiaxpy.input_reader import InputReader
 
 
+def __get_dot_product(L_inv):
+    return dot(L_inv.T, L_inv)
+
 def __get_inv_cholesky_decomp_lower(xp_errors, xp_correlation_matrix):
     L = cholesky(xp_correlation_matrix, lower=True)
     # Invert lower triangular matrix.
@@ -16,10 +19,15 @@ def __get_inv_cholesky_decomp_lower(xp_errors, xp_correlation_matrix):
 def get_inverse_covariance_matrix(input_object=None, band=None):
     band = band.lower()
     parsed_input_data, extension = InputReader(input_object, cholesky)._read()
-    xp_errors = parsed_input_data[f'{band}_coefficient_errors'].iloc[0]
-    xp_correlation_matrix = parsed_input_data[f'{band}_coefficient_correlations'].iloc[0]
-    L_inv = __get_inv_cholesky_decomp_lower(xp_errors, xp_correlation_matrix)
-    return dot(L_inv.T, L_inv)
+    xp_errors = parsed_input_data[f'{band}_coefficient_errors']
+    xp_correlation_matrix = parsed_input_data[f'{band}_coefficient_correlations']
+    L_inv_iterable = map(__get_inv_cholesky_decomp_lower, xp_errors, xp_correlation_matrix)
+    output = list(map(__get_dot_product, L_inv_iterable))
+    if len(output) > 1:
+        return output
+    elif len(output) == 1:
+        return output[0]
+
 
 def get_chi2(residuals, L_inv):
     x = dot(L_inv.T, residuals)

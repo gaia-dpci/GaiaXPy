61152e821a62fec403ec412683c29da3053ec4ec
druzm <dr540@ast.cam.ac.uk>
2022-08-02 16:24:22 +0100

    simple Cholesky test

---
+++ tests/test_cholesky/test_cholesky.py
@@ -0,0 +1,16 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from os.path import join
+from gaiaxpy import get_inverse_covariance_matrix
+from tests.files import files_path
+
+
+class TestCholesky(unittest.TestCase):
+
+    def test_inverse_covariance_matrix_file_from_notebook(self):
+        f = join(files_path, 'cholesky', '18Sco-XP-spectra.csv')
+        inverse = get_inverse_covariance_matrix(f)
+        # Load solution
+        solution = np.loadtxt(join(files_path, 'cholesky', 'nb_bp_get_inv_cov_mat.txt'))
+        npt.assert_array_equal(inverse, solution)

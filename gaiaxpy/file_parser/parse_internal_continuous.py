ff12d05a7a3f23a7b8b880efbe02d8f807486cca
druzm <dr540@ast.cam.ac.uk>
2022-08-03 15:13:05 +0100

    refactor AVRO reader

--- gaiaxpy/file_parser/parse_internal_continuous.py
+++ gaiaxpy/file_parser/parse_internal_continuous.py
@@ -4,26 +4,20 @@ parse_internal_continuous.py
 Module to parse input files containing internally calibrated continuous spectra.
 """
 
+import re
 import numpy as np
 import pandas as pd
-import re
 from fastavro import reader
 from astropy.io.votable import parse_single_table
+from .cast import _cast
 from .parse_generic import GenericParser
 from .utils import _csv_to_avro_map, _get_from_dict
 from gaiaxpy.core import array_to_symmetric_matrix
 from gaiaxpy.file_parser import DataMismatchError
-from .cast import _cast
-
-# Avoid warning, false positive
-pd.options.mode.chained_assignment = None
 
 # Columns that contain arrays (as strings)
-array_columns = [
-    'bp_coefficients',
-    'bp_coefficient_errors',
-    'rp_coefficients',
-    'rp_coefficient_errors']
+array_columns = ['bp_coefficients', 'bp_coefficient_errors', \
+                 'rp_coefficients', 'rp_coefficient_errors']
 # Pairs of the form (matrix_size (N), values_to_put_in_matrix) for columns
 # that contain matrices as strings
 matrix_columns = [('bp_n_parameters', 'bp_coefficient_correlations'),
@@ -91,11 +85,11 @@ class InternalContinuousParser(GenericParser):
             values_to_df.append(row)
         df = pd.DataFrame(values_to_df, columns=columns)
         if matrix_columns is not None:
-            for index, row in df.iterrows():
                 for size_column, values_column in matrix_columns:
                     try:
-                        df[values_column][index] = array_to_symmetric_matrix(
-                            row[values_column], df[size_column][index].astype(int))
+                        df[values_column] = df.apply(lambda row: \
+                        array_to_symmetric_matrix(row[values_column], row[size_column]), \
+                        axis=1)
                     # Value can be NaN when a band is not present
                     except IndexError:
                         continue
@@ -111,47 +105,35 @@ class InternalContinuousParser(GenericParser):
         Returns:
             DataFrame: Pandas DataFrame representing the AVRO file.
         """
-        def _avro_file_to_df(avro_file):
-            records_list = []
+        def __process_record(record):
+            return {key: np.array(_get_from_dict(record, _csv_to_avro_map[key])) \
+                    if isinstance(_get_from_dict(record, _csv_to_avro_map[key]), \
+                    list) else _get_from_dict(record, _csv_to_avro_map[key]) for \
+                    key in _csv_to_avro_map.keys()}
+        def __yield_records(avro_file):
             f = open(avro_file, 'rb')
             avro_reader = reader(f)
             record = avro_reader.next()
             while record:
                 try:
-                    current_record = {}
-                    for key in _csv_to_avro_map.keys():
-                        # Access the record and get the value corresponding to the key
-                        # If the record is a list, it must be converted to numpy array
-                        value = _get_from_dict(record, _csv_to_avro_map[key])
-                        if isinstance(value, list):
-                            value = np.array(value)
-                        # Add this value to a dictionary which represents the current
-                        # record
-                        current_record[key] = value
-                    # Append this record to the global list
-                    records_list.append(current_record)
+                    current_record = __process_record(record)
+                    yield current_record
                 except KeyError:
                     raise KeyError("Keys in the input file don't match the expected ones.")
                 try:
-                    # Move onto the next record
                     record = avro_reader.next()
                 except StopIteration:
-                    # Reached end on file, close it and break
                     f.close()
                     break
-            # Records to DataFrame
-            return pd.DataFrame(records_list)
-        # Pairs of the form (matrix_size (N), values_to_put_in_matrix) for columns
-        # that contain matrices as strings
+        df = pd.DataFrame(__yield_records(avro_file))
+        # Pairs of the form (matrix_size (N), values_to_put_in_matrix)
         to_matrix_columns = [('bp_n_parameters', 'bp_coefficient_covariances'),
                              ('rp_n_parameters', 'rp_coefficient_covariances')]
-        df = _avro_file_to_df(avro_file)
-        for index, row in df.iterrows():
-            for size_column, values_column in to_matrix_columns:
-                try:
-                    df[values_column][index] = array_to_symmetric_matrix(
-                        row[values_column], df[size_column][index].astype(int))
-                # Value can be NaN when a band is not present
-                except TypeError:
-                    continue
+        for size_column, values_column in to_matrix_columns:
+            try:
+                df[values_column] = df.apply(lambda row: \
+                array_to_symmetric_matrix(row[values_column], row[size_column]), \
+                axis=1)
+            except TypeError:
+                continue # Value can be NaN when a band is not present
         return _cast(df)

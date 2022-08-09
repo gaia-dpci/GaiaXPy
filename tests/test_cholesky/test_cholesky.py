c6402207b4f44ea88e5f459c881f538e45fa074d
druzm <dr540@ast.cam.ac.uk>
2022-08-09 16:13:23 +0100

    Cholesky returns DataFrames

--- tests/test_cholesky/test_cholesky.py
+++ tests/test_cholesky/test_cholesky.py
@@ -20,21 +20,24 @@ def str_to_np_array(array):
 class TestCholesky(unittest.TestCase):
 
     def test_inverse_covariance_matrix_from_file(self):
-        inverse = get_inverse_covariance_matrix(f, band='bp')
-        npt.assert_array_almost_equal(inverse, solution)
+        inverse_df = get_inverse_covariance_matrix(f, band='bp')
+        inverse_cov = inverse_df['bp_inverse_covariance'].iloc[0]
+        npt.assert_array_almost_equal(inverse_cov, solution)
 
     def test_inverse_covariance_matrix_from_df_str(self):
         df = pd.read_csv(f)
-        inverse = get_inverse_covariance_matrix(df, band='bp')
-        npt.assert_array_almost_equal(inverse, solution)
+        inverse_df = get_inverse_covariance_matrix(df, band='bp')
+        inverse_cov = inverse_df['bp_inverse_covariance'].iloc[0]
+        npt.assert_array_almost_equal(inverse_cov, solution)
 
     def test_inverse_covariance_matrix_file_from_df_numpy_array(self):
         df = pd.read_csv(f)
         # Correlations and error should be numpy array
         df['bp_coefficient_correlations'] = df['bp_coefficient_correlations'].map(str_to_np_array)
         df['bp_coefficient_errors'] = df['bp_coefficient_errors'].map(str_to_np_array)
-        inverse = get_inverse_covariance_matrix(df, band='bp')
-        npt.assert_array_almost_equal(inverse, solution)
+        inverse_df = get_inverse_covariance_matrix(df, band='bp')
+        inverse_cov = inverse_df['bp_inverse_covariance'].iloc[0]
+        npt.assert_array_almost_equal(inverse_cov, solution)
 
     def test_inverse_covariance_matrix_file_from_df_numpy_matrix(self):
         df = pd.read_csv(f)
@@ -43,5 +46,6 @@ class TestCholesky(unittest.TestCase):
         df['bp_coefficient_correlations'] = df.apply(lambda row: array_to_symmetric_matrix(row['bp_coefficient_correlations'], \
                                                      row['bp_n_parameters']), axis=1)
         df['bp_coefficient_errors'] = df['bp_coefficient_errors'].map(str_to_np_array)
-        inverse = get_inverse_covariance_matrix(df, band='bp')
-        npt.assert_array_almost_equal(inverse, solution)
+        inverse_df = get_inverse_covariance_matrix(df, band='bp')
+        inverse_cov = inverse_df['bp_inverse_covariance'].iloc[0]
+        npt.assert_array_almost_equal(inverse_cov, solution)

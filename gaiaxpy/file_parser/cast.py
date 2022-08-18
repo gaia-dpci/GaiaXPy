45847decead3d85fc76f3c228887b8cd78f79a92
druzm <dr540@ast.cam.ac.uk>
2022-08-18 09:52:15 +0100

    modify cast to replace masked arrays

--- gaiaxpy/file_parser/cast.py
+++ gaiaxpy/file_parser/cast.py
@@ -5,55 +5,70 @@ Module to cast the data after parsing.
 """
 import numpy as np
 from numpy import dtype
-from numpy.ma import MaskError
+from numpy.ma import MaskError, getdata
+from pandas.errors import IntCastingNaNError
 
-# Fields of the AVRO file and other formats.
-_type_map = {'source_id': dtype('int64'),
-            'solution_id': dtype('int64'),
-            'rp_n_parameters': dtype('int64'),
-            'bp_n_parameters': dtype('int64'),
-            'rp_n_rejected_measurements': dtype('int64'),
-            'bp_n_rejected_measurements': dtype('int64'),
-            'rp_n_measurements': dtype('int64'),
-            'bp_n_measurements': dtype('int64'),
-            'rp_standard_deviation': dtype('float64'),
-            'bp_standard_deviation': dtype('float64'),
-            'rp_num_of_transits': dtype('int64'),
-            'bp_num_of_transits': dtype('int64'),
-            'rp_num_of_blended_transits': dtype('int64'),
-            'bp_num_of_blended_transits': dtype('int64'),
-            'rp_num_of_contaminated_transits': dtype('int64'),
-            'bp_num_of_contaminated_transits': dtype('int64'),
-            'rp_coefficients': dtype('O'),
-            'bp_coefficients': dtype('O'),
-            'rp_coefficient_covariances': dtype('O'),
-            'bp_coefficient_covariances': dtype('O'),
-            'rp_degrees_of_freedom': dtype('int64'),
-            'bp_degrees_of_freedom': dtype('int64'),
-            'rp_n_relevant_bases': dtype('int64'),
-            'bp_n_relevant_bases': dtype('int64'),
-            'rp_basis_function_id': dtype('int64'),
-            'bp_basis_function_id': dtype('int64'),
-            'rp_chi_squared': dtype('float64'),
-            'bp_chi_squared': dtype('float64'),
-            'rp_coefficient_errors': dtype('O'),
-            'bp_coefficient_errors': dtype('O'),
-            'rp_coefficient_correlations': dtype('O'),
-            'bp_coefficient_correlations': dtype('O'),
-            'rp_relative_shrinking': dtype('float64'),
-            'bp_relative_shrinking': dtype('float64')}
+# Fields of all formats including AVRO.
+__type_map = {'source_id': 'int64',
+              'solution_id': 'int64',
+              'rp_n_parameters': 'int64',
+              'bp_n_parameters': 'int64',
+              'rp_n_rejected_measurements': 'int64',
+              'bp_n_rejected_measurements': 'int64',
+              'rp_n_measurements': 'int64',
+              'bp_n_measurements': 'int64',
+              'rp_standard_deviation': 'float64',
+              'bp_standard_deviation': 'float64',
+              'rp_num_of_transits': 'int64',
+              'bp_num_of_transits': 'int64',
+              'rp_num_of_blended_transits': 'int64',
+              'bp_num_of_blended_transits': 'int64',
+              'rp_num_of_contaminated_transits': 'int64',
+              'bp_num_of_contaminated_transits': 'int64',
+              'rp_coefficients': 'O',
+              'bp_coefficients': 'O',
+              'rp_coefficient_covariances': 'O',
+              'bp_coefficient_covariances': 'O',
+              'rp_degrees_of_freedom': 'int64',
+              'bp_degrees_of_freedom': 'int64',
+              'rp_n_relevant_bases': 'int64',
+              'bp_n_relevant_bases': 'int64',
+              'rp_basis_function_id': 'int64',
+              'bp_basis_function_id': 'int64',
+              'rp_chi_squared': 'float64',
+              'bp_chi_squared': 'float64',
+              'rp_coefficient_errors': 'O',
+              'bp_coefficient_errors': 'O',
+              'rp_coefficient_correlations': 'O',
+              'bp_coefficient_correlations': 'O',
+              'rp_relative_shrinking': 'float64',
+              'bp_relative_shrinking': 'float64'}
 
+def __replace_masked_constant(value, itype):
+    if isinstance(value, np.ma.core.MaskedConstant):
+        return float('NaN')
+    else:
+        return value
+
+def __replace_masked_array(value, itype):
+    if (isinstance(value, np.ma.core.MaskedArray) and getdata(value).size == 0) \
+        or (isinstance(value, float) and value == 0.0):
+        return np.array([])
+    else:
+        return value
 
 def _cast(df):
     """
     Cast types to the defined ones to standardise the different input formats.
-
     Args:
         df (DataFrame): a DataFrame with parsed data from input files.
     """
-    for column in df.columns:
+    for column, type_value in __type_map.items():
         try:
-            df[column] = df[column].astype(_type_map[column])
+            if type_value == 'O':
+                df[column] = df[column].apply(lambda x: __replace_masked_array(x, type_value))
+            else:
+                df[column] = df[column].astype(type_value)
         except KeyError:
             continue  # Not every key is available in every case
         except ValueError as err:

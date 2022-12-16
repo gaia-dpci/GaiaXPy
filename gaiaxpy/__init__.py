c0a31e57e926e108a432830691e050ce3b7a1e64
druzm <dr540@ast.cam.ac.uk>
2022-12-16 09:47:19 +0000

    Create load additional systems function

--- gaiaxpy/__init__.py
+++ gaiaxpy/__init__.py
@@ -4,7 +4,8 @@ from .converter.converter import convert
 from .core.dispersion_function import pwl_to_wl, wl_to_pwl, pwl_range, wl_range
 from .error_correction.error_correction import apply_error_correction
 from .generator.generator import generate
+from .generator.photometric_system import load_additional_systems
 from .generator.photometric_system import PhotometricSystem
 from .plotter.plot_spectra import plot_spectra
 
-__version__ = '1.2.3'
+__version__ = '1.2.3.dev'

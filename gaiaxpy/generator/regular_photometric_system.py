ba72dfbd0c253f0b5e031e4bf40076211797b04b
druzm <dr540@ast.cam.ac.uk>
2022-12-16 10:14:14 +0000

    Add config file parameter

--- gaiaxpy/generator/regular_photometric_system.py
+++ gaiaxpy/generator/regular_photometric_system.py
@@ -3,20 +3,23 @@ regular_photometric_system.py
 ====================================
 Module to represent a regular photometric system.
 """
+from os.path import join
 
 from .internal_photometric_system import InternalPhotometricSystem
+from ..config.paths import config_path
 
 
 class RegularPhotometricSystem(InternalPhotometricSystem):
 
-    def __init__(self, name):
+    def __init__(self, name, config_file=None):
         """
         A photometric system is defined by the set of bands available.
 
         Args:
             name (str): Name of the PhotometricSystem
         """
-        super().__init__(name)
+        config_file = join(config_path, 'config.ini') if not config_file else config_file
+        super().__init__(name, config_file)
 
     def _correct_flux(self, flux):
         return flux

aa60028d590d3a467871b59b051dc1f0454868b9
druzm <dr540@ast.cam.ac.uk>
2022-12-23 20:08:28 +0000

    Tests need updating

--- tests/test_core/test_config.py
+++ tests/test_core/test_config.py
@@ -51,16 +51,3 @@ class TestConfig(unittest.TestCase):
         system = InternalPhotometricSystem('Stromgren_Std')
         self.assertIsInstance(system.offsets, ndarray)
         npt.assert_array_equal(system.offsets, np.zeros(3))
-
-
-'''
-def test_additional_config(tmpdir):
-    # TODO: Should use a temporary path
-    config_file = join(tmpdir, '.testrc')
-    load_additional_systems(filters_path=join(files_path, 'additional_filters'), config_file=config_file)
-    system = 'test'
-    bp_model = 'v375wi'
-    rp_model = 'v142r'
-    file_path = get_file('filter', 'filter', system, bp_model, rp_model, config_file=config_file)
-    # self.assertEqual(file_path, join(filters_path, basename(file_path)))
-'''

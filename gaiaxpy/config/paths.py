fd4f5b4ded4e998c7ffcd00dc178330ca8aacbd1
druzm <dr540@ast.cam.ac.uk>
2022-12-23 14:44:50 +0000

    Add config file variable

--- gaiaxpy/config/paths.py
+++ gaiaxpy/config/paths.py
@@ -1,4 +1,5 @@
 from os.path import abspath, dirname, join
 
-config_path = abspath(join(dirname(__file__)))
+config_path = abspath(dirname(__file__))
+config_file = join(config_path, 'config.ini')
 filters_path = join(config_path, 'filters')

263ef3b5058b261b89028b778a9eb67965a40cac
druzm <dr540@ast.cam.ac.uk>
2022-12-23 18:33:18 +0000

    Build enum with built-in and additional systems

--- gaiaxpy/generator/photometric_system.py
+++ gaiaxpy/generator/photometric_system.py
@@ -4,29 +4,21 @@ photometric_system.py
 Module for the management of photometric systems.
 """
 
+from configparser import ConfigParser
 from os.path import exists
 
-from aenum import Enum, extend_enum
+from aenum import Enum
 
 from gaiaxpy.core.generic_functions import _get_built_in_systems, _is_built_in_system
-from .config import _CFG_FILE_PATH, get_yes_no_answer, create_config, get_additional_filters_names
+from .config import _CFG_FILE_PATH, create_config, get_additional_filters_names
 from .regular_photometric_system import RegularPhotometricSystem
 from .standardised_photometric_system import StandardisedPhotometricSystem
+from .utils import get_yes_no_answer
 
 
-def _get_available_systems(config_file=None):
-    """
-    Get the available photometric systems according to the
-    package configuration.
-
-    Returns:
-        str: A string containing the names of the photometric
-             systems separated by spaces.
-    """
-    built_in_systems = _get_built_in_systems()
-    # Try to load the configuration and see whether more systems have been defined
-    additional_systems = get_additional_filters_names(config_file)
-    return built_in_systems + additional_systems
+def get_available_systems():
+    systems_list = _get_available_systems()
+    return ', '.join(systems_list)
 
 
 class AutoName(Enum):
@@ -46,8 +38,7 @@ class AutoName(Enum):
     def get_offsets(self):
         return self.value.offsets
 
-    def get_filter_version(self):
-        # TODO: Does not currently work
+    def get_version(self):
         return self.value.version
 
 
@@ -66,21 +57,39 @@ def _system_is_standard(system_name):
 
 
 def create_system(name, systems_path=None):
-    if _system_is_standard(name):
-        return StandardisedPhotometricSystem(name)
-    else:
-        return RegularPhotometricSystem(name, systems_path)
+    return StandardisedPhotometricSystem(name, systems_path) if _system_is_standard(name)\
+        else RegularPhotometricSystem(name, systems_path)
 
 
-def get_available_systems():
-    systems_list = _get_available_systems()
-    return ', '.join(systems_list)
+def _get_available_systems(config_file=None):
+    """
+    Get the available photometric systems according to the
+    package configuration.
+
+    Returns:
+        str: A string containing the names of the photometric
+             systems separated by spaces.
+    """
+    built_in_systems = _get_built_in_systems()
+    # Try to load the configuration and see whether more systems have been defined
+    additional_systems = get_additional_filters_names(config_file)
+    return built_in_systems + additional_systems
 
 
-system_tuples = [(s, create_system(s, None)) if _is_built_in_system(s) else (s, create_system(s, _CFG_FILE_PATH))
-                 for s in _get_available_systems(_CFG_FILE_PATH)]
+def _get_system_tuples():
+    return [(s, create_system(s, None)) if _is_built_in_system(s) else (s, create_system(s, _CFG_FILE_PATH)) for s in
+            _get_available_systems(_CFG_FILE_PATH)]
+
+
+system_tuples = _get_system_tuples()
 PhotometricSystem = AutoName('PhotometricSystem', system_tuples)
 PhotometricSystem.get_available_systems = get_available_systems
+
+
+def get_current_filters_path():
+    _config_parser = ConfigParser()
+    _config_parser.read(_CFG_FILE_PATH)
+    return _config_parser['filter']['filters_dir']
 
 
 def load_additional_systems(_filters_path=None):
@@ -90,8 +99,9 @@ def load_additional_systems(_filters_path=None):
     Args:
         _filters_path (str): Path to directory containing the additional filter files.
     """
-    config_file = _CFG_FILE_PATH
-    __load_additional_systems(_filters_path, config_file)
+    updated_enum = __load_additional_systems(_filters_path, _CFG_FILE_PATH)
+    updated_enum.get_available_systems = get_available_systems
+    return updated_enum
 
 
 def __load_additional_systems(_filters_path=None, config_file=None):
@@ -102,15 +112,12 @@ def __load_additional_systems(_filters_path=None, config_file=None):
         _filters_path (str): Path to directory containing the additional filter files.
         config_file (str): Path to configuration file where the path to the additional filter files will be stored.
     """
-    config_file = _CFG_FILE_PATH if not config_file else config_file
+    config_file = config_file if config_file else _CFG_FILE_PATH
     if exists(config_file):
-        print('A path for additional filters has already been defined.')
-        get_yes_no_answer('Do you want to redefine the path? [y/n]: ', create_config, None)
+        print(f'A path for additional filters has already been defined. The current path is '
+              f'{get_current_filters_path()}')
+        get_yes_no_answer('Do you want to redefine the path? [[y]/n]: ', yes_action=create_config, no_action=None,
+                          yes_args=_filters_path)
     else:
         create_config(_filters_path, config_file)
-    # Re-create AutoEnum
-    global PhotometricSystem
-    new_system_tuples = [(s, create_system(s, config_file)) for s in _get_available_systems(config_file) if not
-    _is_built_in_system(s)]
-    for name, value in new_system_tuples:
-        extend_enum(PhotometricSystem, name, value)
+    return AutoName('PhotometricSystem', _get_system_tuples())

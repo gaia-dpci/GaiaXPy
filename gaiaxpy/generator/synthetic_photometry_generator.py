423727f8f058698ec3db1e049341783a9a573edd
druzm <dr540@ast.cam.ac.uk>
2022-12-23 18:35:00 +0000

    Improve code

--- gaiaxpy/generator/synthetic_photometry_generator.py
+++ gaiaxpy/generator/synthetic_photometry_generator.py
@@ -20,43 +20,34 @@ config_parser.read(path.join(config_path, 'config.ini'))
 
 class SyntheticPhotometryGenerator(object):
 
-    def _generate(input_object, photometric_system, output_file, output_format, save_file,
-                  bp_model='v375wi', rp_model='v142r'):
+    def _generate(input_object, photometric_system, output_file, output_format, save_file, bp_model='v375wi',
+                  rp_model='v142r'):
         raise ValueError('Method not defined for base class.')
 
     def _get_sampled_basis_functions(self, xp_sampling, xp_sampling_grid):
-        sampled_basis_func = {}
-        for band in BANDS:
-            sampled_basis_func[band] = SampledBasisFunctions.from_design_matrix(
-                xp_sampling_grid, xp_sampling[band])
-        return sampled_basis_func
+        return {band: SampledBasisFunctions.from_design_matrix(xp_sampling_grid, xp_sampling[band]) for band in BANDS}
 
     def _create_photometry_list(self, parsed_input_data, photometric_system, sampled_basis_func, xp_merge):
-        photometry_gen = (_generate_synthetic_photometry(row, sampled_basis_func, xp_merge, photometric_system) \
-                          for index, row in parsed_input_data.iterrows())
-        return photometry_gen
+        return (_generate_synthetic_photometry(row, sampled_basis_func, xp_merge, photometric_system) for index, row in
+                parsed_input_data.iterrows())
 
 
 def _generate_synthetic_photometry(row, design_matrix, merge, photometric_system):
     """
-    Create the synthetic photometry from the input continuously-represented
-    mean spectrum and design matrix.
+    Create the synthetic photometry from the input continuously-represented mean spectrum and design matrix.
 
     Args:
-        row (DataFrame): Single row in a DataFrame containing the entry
-            for one source in the mean spectra file. This will include columns for
-            both bands (although one could be missing).
-        design_matrix (ndarray): 2D array containing the basis functions
-            sampled for the specific photometric system.
-        merge (dict): Dictionary containing an array of weights per BP and one for RP.
-            These have one value per sample and define the contributions from BP and RP
-            to the joined absolute spectrum.
+        row (DataFrame): Single row in a DataFrame containing the entry for one source in the mean spectra file. This
+            will include columns for both bands (although one could be missing).
+        design_matrix (ndarray): 2D array containing the basis functions sampled for the specific photometric system.
+        merge (dict): Dictionary containing an array of weights per BP and one for RP. These have one value per sample
+            and define the contributions from BP and RP to the joined absolute spectrum.
         photometric_system (obj): Photometric system object containing the zero-points.
 
     Returns:
         SingleSyntheticPhotometry: The output synthetic photometry.
     """
-    cont_dict = {}
+    cont_dict = dict()
     for band in BANDS:
         covariance_matrix = _get_covariance_matrix(row, band)
         if covariance_matrix is not None:

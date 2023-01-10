db4655df01c7b5933c636476041a887d35a540c2
druzm <dr540@ast.cam.ac.uk>
2023-01-10 14:56:15 +0000

    Check if the config file contains the expected key before assuming a path has been previously defined

--- gaiaxpy/generator/photometric_system.py
+++ gaiaxpy/generator/photometric_system.py
@@ -11,7 +11,7 @@ from os.path import exists
 from aenum import Enum
 
 from gaiaxpy.core.generic_functions import _get_built_in_systems, _is_built_in_system
-from .config import _CFG_FILE_PATH, create_config, get_additional_filters_names
+from .config import _CFG_FILE_PATH, create_config, get_additional_filters_names, contains_filter_key
 from .regular_photometric_system import RegularPhotometricSystem
 from .standardised_photometric_system import StandardisedPhotometricSystem
 from .utils import get_yes_no_answer
@@ -106,6 +106,7 @@ def load_additional_systems(_systems_path=None):
     """
     updated_enum = __load_additional_systems(_systems_path, _CFG_FILE_PATH)
     updated_enum.get_available_systems = get_available_systems
+    print("Systems loaded. Use PhotometricSystem.get_available_systems() to get the names of the current systems.")
     return updated_enum
 
 
@@ -118,7 +119,7 @@ def __load_additional_systems(_filters_path=None, config_file=None):
         config_file (str): Path to configuration file where the path to the additional filter files will be stored.
     """
     config_file = config_file if config_file else _CFG_FILE_PATH
-    if exists(config_file):
+    if exists(config_file) and contains_filter_key(config_file):
         print(f'A path for additional filters has already been defined. The current path is '
               f'{get_current_filters_path()}')
         get_yes_no_answer('Do you want to redefine the path? [[y]/n]: ', yes_action=create_config, no_action=None,
@@ -126,3 +127,21 @@ def __load_additional_systems(_filters_path=None, config_file=None):
     else:
         create_config(_filters_path, config_file)
     return AutoName('PhotometricSystem', _get_system_tuples())
+
+
+def remove_additional_systems():
+    """
+    Remove previously loaded additional photometric systems. If no additional systems have been added, no changes will
+    be made.
+
+    Returns:
+        Enum: PhotometricSystem object corresponding to an enumeration of the updated available systems.
+    """
+    if exists(_CFG_FILE_PATH):
+        remove(_CFG_FILE_PATH)
+        print('Additional systems configuration successfully removed.')
+    else:
+        print('No additional configuration exists.')
+    _PhotometricSystem = AutoName('PhotometricSystem', _get_system_tuples())
+    _PhotometricSystem.get_available_systems = get_available_systems
+    return _PhotometricSystem

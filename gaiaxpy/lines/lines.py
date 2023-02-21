f6410900c6975537499e3907ab1f35dab9ca0136
zuzannakr <zuzanna.kostrzewa@gmail.com>
2023-02-21 13:07:18 +0100

    Update lines.py

--- gaiaxpy/lines/lines.py
+++ gaiaxpy/lines/lines.py
@@ -1,7 +1,8 @@
 
 import numpy as np
 
-from gaiaxpy.config.paths import config_path
+from gaiaxpy.lines.linefinder import _wl_to_pwl
+from gaiaxpy.core.satellite import BANDS, BP_WL, RP_WL
 
 # local library of lines
 _qsoline_names = ['Ly_alpha','C IV','C III]','Mg II','H_beta','H_alpha']
@@ -11,19 +12,13 @@ _starline_names = ['H_beta','H_alpha','He I_1','He I_2','He I_3']
 _starlines = [486.268,656.461,447.3,587.7,706.7]
 
 
-_rp_dispersion = np.loadtxt(config_path + '/rpC03_v142r_dispersion.csv', delimiter = ',')
-_rp_wvl = _rp_dispersion[0]
-_rp_pwl = _rp_dispersion[1]
-
-_bp_dispersion = np.loadtxt(config_path + '/bpC03_v375wi_dispersion.csv', delimiter = ',')
-_bp_wvl = _bp_dispersion[0]
-_bp_pwl = _bp_dispersion[1]
 
 class Lines():
   
-  def __init__(self, src_type, xp):
-    self.src_type = src_type
+  def __init__(self, xp, src_type, dispersion):
     self.xp = xp
+    self.src_type = src_type
+    self.disp = dispersion
 
   def get_lines_pwl(self, zet=0., userlines=None): 
 
@@ -37,18 +32,21 @@ class Lines():
         inputlines = _qsoline
         inputlinenames = _qsoline_names
     else:
-       inputlines = userlines[0]
-       inputlinenames = userlines[1]
-      
-
-      
-      # redshifted lines in wavelength
+      inputlines = userlines[0]
+      inputlinenames = userlines[1]
+    
+    inputlines = np.array(inputlines)
+    inputlinenames = np.array(inputlinenames)
+    
+    # redshifted lines in wavelength
     inputlines = inputlines * (1. + zet)
-    if self.xp == 'BP':
-         line_pwl = np.interp(inputlines[(_bp_wvl[0]<inputlines)&(inputlines<_bp_wvl[-1])], _bp_wvl, _bp_pwl)
-         lines = (np.asarray(inputlinenames)[(_bp_wvl[0]<inputlines)&(inputlines<_bp_wvl[-1])], line_pwl)
-    elif self.xp == 'RP':
-         line_pwl = np.interp(inputlines[(_rp_wvl[0]<inputlines)&(inputlines<_rp_wvl[-1])], _rp_wvl, _rp_pwl)
-         lines = (np.asarray(inputlinenames)[(_rp_wvl[0]<inputlines)&(inputlines<_rp_wvl[-1])], line_pwl)
-
+    if self.xp == BANDS.bp:
+         mask = (inputlines)>BP_WL.low)&(inputlines<BP_WL.high)  # mask outside wavelength range range
+         line_pwl = _wl_to_pwl(inputlines[mask], self.disp)
+         lines = (np.asarray(inputlinenames)[mask], line_pwl)
+    elif self.xp == BANDS.rp:
+         mask = (inputlines)>RP_WL.low)&(inputlines<RP_WL.high)  # mask outside wavelength range range
+         line_pwl = _wl_to_pwl(inputlines[mask], self.disp)
+         lines = (np.asarray(inputlinenames)[mask], line_pwl)
+        
     return lines  

b16e286fdf46d2dd66d260eabc971de1d5309979
druzm <dr540@ast.cam.ac.uk>
2023-05-10 10:06:18 +0100

    Improve solution files parsing

--- tests/test_lines/test_linefinder.py
+++ tests/test_lines/test_linefinder.py
@@ -17,38 +17,34 @@ input_file = join(continuous_path, 'XP_CONTINUOUS_RAW.csv')
 # Input file with xp continuous spectra (with missing BP)
 input_file_nobp = join(continuous_path, 'XP_CONTINUOUS_RAW_with_missing_BP.csv')
 
-lines_dtypes = [('line_name', 'U12'), ('wavelength_nm', 'f8'), ('flux', 'f8'), ('depth', 'f8'), ('width', 'f8'),
-                ('significance', 'f8'), ('sig_pwl', 'f8')]
-
-extrema_dtypes = [('line_name', '<U12'), ('wavelength_nm', '<f8'), ('flux', '<f8'), ('depth', '<f8'), ('width', '<f8'),
-                  ('significance', '<f8'), ('sig_pwl', '<f8')]
-
-dtypes_dict = {'lines': lines_dtypes, 'extrema': extrema_dtypes}
+dtypes = [('line_name', 'U12'), ('wavelength_nm', 'f8'), ('flux', 'f8'), ('depth', 'f8'), ('width', 'f8'),
+          ('significance', 'f8'), ('sig_pwl', 'f8')]
 
 _rtol, _atol = 1e-7, 1e-7
 
 
-def custom_comparison(df1, df2, column):
-    dtypes = dtypes_dict[column]
-    df1_lines = df1[column].values
-    df2_lines = df2[column].values
-    if len(df1_lines) == len(df2_lines):
-        for lines1, lines2 in zip(df1_lines, df2_lines):
-            if len(lines1) == len(lines2):
-                for line1, line2 in zip(lines1, lines2):
+def custom_comparison(df_computed, df_solution, column):
+    df_computed_lines = df_computed[column].values
+    df_solution_lines = df_solution[column].values
+    if len(df_computed_lines) == len(df_solution_lines):
+        for computed_lines, solution_lines in zip(df_computed_lines, df_solution_lines):
+            if len(computed_lines) == len(solution_lines):
+                for computed_line, solution_line in zip(computed_lines, solution_lines):
                     # Convert line to dictionary
-                    d1 = {key: line1[key] for key, _ in dtypes}
-                    d2 = {key: line2[key] for key, _ in dtypes}
+                    d1 = {key: computed_line[key] for key, _ in dtypes}
+                    d2 = {key: solution_line[key] for key, _ in dtypes}
                     line_name1 = d1.pop('line_name')
                     line_name2 = d2.pop('line_name')
                     assert line_name1 == line_name2
                     assert d1 == pytest.approx(d2, rel=_rtol, abs=_atol, nan_ok=True)
-        return
-    raise ValueError('DataFrames are different.')
+            else:
+                raise ValueError('DataFrames are different.')
+    else:
+        raise ValueError('DataFrames are different.')
 
 
 # Define the converter function
-def str_to_array_rec(input_str, dtypes):
+def str_to_array_rec(input_str):
     lst = eval(input_str)
     lst = [tuple(element) for element in lst]
     output = [np.array(line, dtype=dtypes) for line in lst]
@@ -56,18 +52,21 @@ def str_to_array_rec(input_str, dtypes):
 
 
 def get_converters(columns):
-    if len(columns) == 1:
-        column = columns[0]
-        dtypes = dtypes_dict[column]
-        return {column: lambda x: str_to_array_rec(x, dtypes)}
-    elif len(columns) > 1:
-        return {column: lambda x: str_to_array(x) for column in columns}
+    if isinstance(columns, str):
+        return {columns: lambda x: str_to_array_rec(x)}
+    elif isinstance(columns, list):
+        if len(columns) == 1:
+            return {columns[0]: lambda x: str_to_array_rec(x)}
+        elif len(columns) > 1:  # Extrema BP and RP case
+            return {column: lambda x: str_to_array(x) for column in columns}
+    raise ValueError("Input doesn't correspond to any of the expected types.")
 
 
 # Results to compare
 solution_folder = 'lines_files'
 found_lines_real = pd.read_csv(join(files_path, solution_folder, 'linefinder_output.csv'),
-                               converters=get_converters('lines'))
+                               converters=get_converters(['lines']))
+
 found_lines_trunc_real = pd.read_csv(join(files_path, solution_folder, 'linefinder_trunc_output.csv'),
                                      converters=get_converters('lines'))
 found_lines_nobp_real = pd.read_csv(join(files_path, solution_folder, 'linefinder_nobp_output.csv'),

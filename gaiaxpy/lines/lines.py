17abe0819bdb248e1802c1cdf489c5cc9386e7e0
druzm <dr540@ast.cam.ac.uk>
2023-05-11 17:50:38 +0100

    Done lines refactoring

--- gaiaxpy/lines/lines.py
+++ gaiaxpy/lines/lines.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from gaiaxpy.core.dispersion_function import wl_to_pwl
 from gaiaxpy.core.satellite import BANDS, BP_WL, RP_WL
+from gaiaxpy.lines.utils import _validate_source_type
 
 # local library of lines
 _qso_line_names = ['Ly_alpha', 'C IV', 'C III]', 'Mg II', 'H_beta', 'H_alpha']
@@ -25,30 +26,32 @@ class Lines:
         Args:
             xp (str): BP or RP.
             src_type (str): Type of sources (star or quasars).
-            user_lines (list): List of lines defined by user.
+            user_lines (list/str): List of lines defined by user.
         """
 
         self.xp = xp
-        self.src_type = src_type
+        self.src_type = _validate_source_type(src_type)
 
-        if user_lines is None:  # get lines from local library
+        if user_lines is None:  # Get lines from local library
             if self.src_type == 'star':
                 input_lines = _star_lines
                 input_line_names = _star_line_names
             elif self.src_type == 'qso':
                 input_lines = _qso_lines
                 input_line_names = _qso_line_names
+            else:
+                raise ValueError("Unknown source type. Valid source types are: 'qso' and 'star'.")
         else:
-            if isinstance(user_lines, list):  # get lines from a list provided by user
+            if isinstance(user_lines, list):  # Get lines from a list provided by user
                 input_lines = user_lines[0]
                 input_line_names = user_lines[1]
-            elif path.isfile(user_lines):  # get lines from a file provided by user
+            elif path.isfile(user_lines):  # Get lines from a file provided by user
                 input_lines, input_line_names = np.loadtxt(user_lines, unpack=True, dtype='f8,U12')
             else:
                 raise ValueError('Input is not corresponding to a list of lines or an existing file.')
 
-        self.inlines = np.array(input_lines)
-        self.inlinenames = np.array(input_line_names)
+        self.in_lines = np.array(input_lines)
+        self.in_line_names = np.array(input_line_names)
 
     def get_lines_pwl(self, zet=0.):
         """
@@ -61,16 +64,16 @@ class Lines:
             list: List of (redshifted) lines in pseudo-wavelengths with their names.
         """
         lines = []
-        # redshifted lines in wavelength
-        in_lines_red = self.inlines * (1. + zet)
+        # Redshifted lines in wavelength
+        in_lines_red = self.in_lines * (1. + zet)
 
         if self.xp == BANDS.bp:
-            mask = (in_lines_red > BP_WL.low) & (in_lines_red < BP_WL.high)  # mask outside wavelength range range
+            mask = (in_lines_red > BP_WL.low) & (in_lines_red < BP_WL.high)  # Mask outside wavelength range range
             line_pwl = wl_to_pwl(self.xp, in_lines_red[mask])
-            lines = (np.asarray(self.inlinenames)[mask], line_pwl)
+            lines = (np.asarray(self.in_line_names)[mask], line_pwl)
         elif self.xp == BANDS.rp:
-            mask = (in_lines_red > RP_WL.low) & (in_lines_red < RP_WL.high)  # mask outside wavelength range range
+            mask = (in_lines_red > RP_WL.low) & (in_lines_red < RP_WL.high)  # Mask outside wavelength range range
             line_pwl = wl_to_pwl(self.xp, in_lines_red[mask])
-            lines = (np.asarray(self.inlinenames)[mask], line_pwl)
+            lines = (np.asarray(self.in_line_names)[mask], line_pwl)
 
         return lines

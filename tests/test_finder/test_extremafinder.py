836df88bc06de7d9c8420fe33aedea4893bc9319
druzm <dr540@ast.cam.ac.uk>
2023-05-12 16:23:30 +0100

    Add all inputs extrema tests

--- tests/test_finder/test_extremafinder.py
+++ tests/test_finder/test_extremafinder.py
@@ -2,6 +2,8 @@ import unittest
 
 import pandas as pd
 
+import pandas.testing as pdt
+
 from gaiaxpy.finder.linefinder import extremafinder
 from tests.files.paths import *
 from tests.utils.utils import custom_void_array_comparison, get_converters
@@ -28,6 +30,9 @@ found_extrema = extremafinder(input_file)
 found_extrema_trunc = extremafinder(input_file, truncation=True)
 found_extrema_nobp = extremafinder(with_missing_bp_csv_file)
 
+isolated_solution = found_extrema_nobp_real[found_extrema_nobp_real['source_id'] == missing_bp_source_id].reset_index(
+    drop=True)
+
 
 class TestExtremaFinder(unittest.TestCase):
 
@@ -49,10 +54,29 @@ class TestExtremaFinder(unittest.TestCase):
 
     # Missing BP source in isolation
     def test_file_input_isolated_missing_bp_source(self):
-        isolated_solution = found_extrema_nobp_real[found_extrema_nobp_real['source_id'] ==
-                                                    missing_bp_source_id].reset_index(drop=True)
         missing_input_files = [missing_bp_csv_file, missing_bp_ecsv_file, missing_bp_fits_file, missing_bp_xml_file,
                                missing_bp_xml_plain_file]
         for _input_file in missing_input_files:
             output = extremafinder(_input_file)
             custom_void_array_comparison(output, isolated_solution, 'extrema', dtypes)
+
+    def test_df_input_with_missing_bp(self):
+        with_missing_df = pd.read_csv(with_missing_bp_csv_file)
+        with_missing_output = extremafinder(with_missing_df)
+        custom_void_array_comparison(with_missing_output, found_extrema_nobp_real, 'extrema', dtypes)
+
+    def test_df_input_isolated_missing_bp(self):
+        isolated_df = pd.read_csv(missing_bp_csv_file)
+        isolated_output = extremafinder(isolated_df)
+        custom_void_array_comparison(isolated_output, isolated_solution, column='extrema', dtypes=dtypes)
+
+    def test_query_input_with_missing_bp(self):
+        query = "SELECT * FROM gaiadr3.gaia_source WHERE source_id IN ('5853498713190525696', '5405570973190252288', " \
+                "'5762406957886626816')"
+        with_missing_output = extremafinder(query)
+        custom_void_array_comparison(with_missing_output, found_extrema_nobp_real.sort_values(
+            'source_id', ignore_index=True), 'extrema', dtypes)
+
+    def test_query_input_isolated_missing_bp(self):
+        isolated_output = extremafinder("SELECT * FROM gaiadr3.gaia_source WHERE source_id IN ('5405570973190252288')")
+        custom_void_array_comparison(isolated_output, isolated_solution, column='extrema', dtypes=dtypes)

f2bb9c6d7a3bb9e011cdc9981960c3722f56af19
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-06-08 15:12:50 +0100

    Rename array_columns var

--- gaiaxpy/file_parser/parse_internal_sampled.py
+++ gaiaxpy/file_parser/parse_internal_sampled.py
@@ -23,4 +23,4 @@ class InternalSampledParser(GenericParser):
         Returns:
             DataFrame: Pandas DataFrame representing the CSV file.
         """
-        return super()._parse_csv(csv_file, array_columns=['flux', 'error'])
+        return super()._parse_csv(csv_file, _array_columns=['flux', 'error'])

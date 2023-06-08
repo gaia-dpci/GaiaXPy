b074f8ab0e8083e2b55db773c6d08056c199406f
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-06-08 02:03:23 +0100

    Speed up correction by not recomputing some variables

--- gaiaxpy/error_correction/error_correction.py
+++ gaiaxpy/error_correction/error_correction.py
@@ -30,57 +30,51 @@ def _get_correctable_systems():
 
 
 def _read_system_table(system):
-    # Read system table
     try:
         correction_factors_path = path.join(correction_tables_path, f'DIDREQ-465-{system}-correction-factors.csv')
         correction_table = pd.read_csv(correction_factors_path, float_precision='high')
-        correction_table['bin_centre'] = (correction_table['min_Gmag_bin'] + correction_table['max_Gmag_bin']) / 2
     except FileNotFoundError:
         raise FileNotFoundError(f'No correction table found for system {system}.')
+    correction_table['bin_centre'] = (correction_table['min_Gmag_bin'] + correction_table['max_Gmag_bin']) / 2
     return correction_table
 
 
 def _get_correction_array(_mag_G_values, system):
-    def __get_correction_array(__mag_G_values, __correction_table):
-        for mag in __mag_G_values:
-            yield _get_correction_factors(mag, __correction_table)
     correction_table = _read_system_table(system)
-    correction_array = __get_correction_array(_mag_G_values, correction_table)
-    correction_array = [sublist for sublist in zip(*correction_array)]
-    return np.array(correction_array)
-
-
-def _get_correction_factors(mag, correction_table):
+    factor_columns = [column for column in correction_table.columns if 'factor_' in column]
     # Min and max range values in the table
     min_value = correction_table['min_Gmag_bin'].iloc[0]
     max_value = correction_table['max_Gmag_bin'].iloc[-1]
-    # Factor columns
-    factor_columns = [column for column in correction_table.columns if 'factor_' in column]
+    correction_array = np.array([_get_correction_factors(mag, correction_table, factor_columns, min_value, max_value)
+                                 for mag in _mag_G_values])
+    return correction_array
+
+
+def _get_correction_factors(mag, correction_table, factor_columns, min_value, max_value):
     # See whether the mag is in the table
     if mag > max_value or isnan(mag):
-        return np.array(correction_table[factor_columns].iloc[-1])
+        return correction_table[factor_columns].iloc[-1].values
     elif mag < min_value:
-        return np.array(correction_table[factor_columns].iloc[0])
-    # Else the mag is in the table
-    min_mag = floor(mag)  # Find the range of the given mag
-    # Extract the row of the table that matches this value
-    range_row = correction_table[correction_table['min_Gmag_bin'] == min_mag].iloc[0]
-    # If the column extracted is the last one, we have no way to extrapolate, so we keep the correction factors
+        return correction_table[factor_columns].iloc[0].values
+    # Find the range of the given mag, and extract the row of the table that matches this value
+    range_row = correction_table[correction_table['min_Gmag_bin'] == floor(mag)].iloc[0]
+    # If the column extracted is the last one, we have no way to extrapolate, so we return the correction factors
     factors = range_row[factor_columns]
     bin_centre = range_row['bin_centre']
+    range_row_max_Gmag_bin = range_row['max_Gmag_bin']
     try:
-        next_range_row = correction_table[correction_table['min_Gmag_bin'] == range_row['max_Gmag_bin']].iloc[0]
-    except:  # TODO: Manage exception properly.
-        return factors
+        next_range_row = correction_table[correction_table['min_Gmag_bin'] == range_row_max_Gmag_bin].iloc[0]
+    except IndexError:
+        return factors.values
     # Now for the rest
     if mag <= bin_centre:
-        return factors
-    # or interpolate
-    elif bin_centre < mag < range_row['max_Gmag_bin']:
+        return factors.values
+    # Or interpolate
+    elif bin_centre < mag < range_row_max_Gmag_bin:
         next_factors = next_range_row[factor_columns]
-        correction_factors = [interp1d(np.array([bin_centre, range_row['max_Gmag_bin']]), np.array([
-            factor, next_factors[index]]))(mag) for index, factor in enumerate(factors)]
-        return np.array(correction_factors)
+        correction_factors = interp1d(np.array([bin_centre, range_row_max_Gmag_bin]),
+                                      np.vstack([factors, next_factors]), axis=0)(mag)
+        return correction_factors
     else:
         raise ValueError('Check the variables being used. The program should never fall in this case.')
 
@@ -88,10 +82,12 @@ def _get_correction_factors(mag, correction_table):
 def _correct_system(system_df, correction_array):
     # Extract error columns
     error_df = system_df[[column for column in system_df.columns if '_error' in column]]
-    # Factors for each column
-    for index, column in enumerate(error_df.columns):
-        error_df[column] = error_df[column] * correction_array[index]
-    return error_df
+    error_df_columns = error_df.columns
+    if len(error_df_columns) != len(correction_array[0]):
+        raise ValueError('DataFrames should have the same number of columns.')
+    error_df = np.array(error_df)
+    product_array = error_df * correction_array
+    return pd.DataFrame(product_array, columns=error_df_columns)
 
 
 # The correction can only be applied for the systems present in the config files
@@ -117,9 +113,18 @@ def apply_error_correction(input_multi_photometry, photometric_system=None, outp
         DataFrame: A DataFrame of all synthetic photometry with corrected errors for the systems for which it is
             possible.
     """
+    return _apply_error_correction(input_multi_photometry, photometric_system=photometric_system,
+                                   output_path=output_path, output_file=output_file, output_format=output_format,
+                                   save_file=save_file)
+
+
+def _apply_error_correction(input_multi_photometry, photometric_system=None, output_path='.',
+                            output_file='output_corrected_photometry', output_format=None, save_file=True,
+                            disable_info=False):
     gaia_system = 'GaiaDr3Vega'
     gaia_G_mag_column = f'{gaia_system}_mag_G'
-    input_multi_photometry, extension = InputReader(input_multi_photometry, apply_error_correction).read()
+    input_multi_photometry, extension = InputReader(input_multi_photometry, apply_error_correction,
+                                                    disable_info=disable_info).read()
     # Validate that it is a multi-photometry, but how? First try below:
     if not gaia_G_mag_column in input_multi_photometry.columns:
         raise ValueError('System Gaia_DR3_Vega, required to apply the error correction is not present in the input'
@@ -130,17 +135,18 @@ def apply_error_correction(input_multi_photometry, photometric_system=None, outp
     # Only correct the systems that can be corrected
     systems = list(set(systems_in_data) & set(__correctable_systems))
     systems_to_skip = set(systems_in_data) - set(__correctable_systems)
+    if systems_to_skip and not disable_info:
+        print()
     for system in systems_to_skip:
         _warning(f'System {system} does not have a correction table. The program will not apply error correction over'
                  f' this system.')
-    # Now we have to apply the correction on each of the systems, but this correction depends on the G band
-    for system in tqdm(systems, desc='Correcting systems', total=len(systems), unit=pbar_units['correction'],
+    for system in tqdm(systems, desc=f'Correcting systems', total=len(systems), unit=pbar_units['correction'],
                        leave=False, colour=pbar_colour):
         system_df = input_multi_photometry[[column for column in input_multi_photometry.columns if
                                             (column.startswith(system) and f'{system}Std' not in column) or
                                             column == gaia_G_mag_column]]
         # Get the correction factors for the mag G column
-        correction_array = _get_correction_array(system_df[gaia_G_mag_column].values, system)
+        correction_array = _get_correction_array(system_df[gaia_G_mag_column], system)
         # Correct error magnitudes
         corrected_system = _correct_system(system_df, correction_array)
         # Apply correction to the original input_multi_photometry

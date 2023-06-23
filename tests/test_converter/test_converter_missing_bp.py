5c72d0b1dd0f02f6634320e1e7de9aca7f02a374
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-06-23 18:09:10 +0100

    Refactor converter missing BP tests

--- tests/test_converter/test_converter_missing_bp.py
+++ tests/test_converter/test_converter_missing_bp.py
@@ -15,59 +15,24 @@ from tests.test_converter.converter_paths import with_missing_solution_df, with_
 
 _rtol, _atol = 1e-7, 1e-7
 
+con_with_missing_input_files = [with_missing_bp_csv_file, with_missing_bp_ecsv_file, with_missing_bp_fits_file,
+                                with_missing_bp_xml_file, with_missing_bp_xml_plain_file]
+con_isolated_missing_input_files = [missing_bp_csv_file, missing_bp_ecsv_file, missing_bp_fits_file, missing_bp_xml_file,
+                                    missing_bp_xml_plain_file]
 
 class TestConverterMissingBPFileInput(unittest.TestCase):
 
-    def test_missing_bp_csv_file(self):
-        # TODO: test sampling
-        output_df, sampling = convert(with_missing_bp_csv_file, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
+    def test_with_missing_bp_file(self):
+        for file in con_with_missing_input_files:
+            output_df, sampling = convert(file, save_file=False)
+            npt.assert_array_equal(sampling, with_missing_solution_sampling)
+            pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
 
-    def test_missing_bp_ecsv_file(self):
-        output_df, sampling = convert(with_missing_bp_ecsv_file, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_fits_file(self):
-        output_df, sampling = convert(with_missing_bp_fits_file, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_xml_file(self):
-        output_df, sampling = convert(with_missing_bp_xml_file, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_xml_plain_file(self):
-        output_df, sampling = convert(with_missing_bp_xml_plain_file, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_csv_file_isolated(self):
-        output_df, sampling = convert(missing_bp_csv_file, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_ecsv_file_isolated(self):
-        output_df, sampling = convert(missing_bp_ecsv_file, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_fits_file_isolated(self):
-        output_df, sampling = convert(missing_bp_fits_file, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_xml_file_isolated(self):
-        output_df, sampling = convert(missing_bp_xml_file, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_xml_plain_file_isolated(self):
-        output_df, sampling = convert(missing_bp_xml_plain_file, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
+    def test_missing_bp_file_isolated(self):
+        for file in con_with_missing_input_files:
+            output_df, sampling = convert(file, save_file=False)
+            pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
+            npt.assert_array_equal(sampling, with_missing_solution_sampling)
 
 
 class TestConverterMissingBPDataFrameInput(unittest.TestCase):
@@ -91,30 +56,20 @@ class TestConverterMissingBPDataFrameInput(unittest.TestCase):
         array_columns = ['bp_coefficients', 'bp_coefficient_errors', 'bp_coefficient_correlations', 'rp_coefficients',
                          'rp_coefficient_errors', 'rp_coefficient_correlations']
         _converters = dict([(column, lambda x: str_to_array(x)) for column in array_columns])
-        df = pd.read_csv(with_missing_bp_csv_file, converters=_converters)
-        output_df, sampling = convert(df, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_array_dataframe_isolated(self):
-        array_columns = ['bp_coefficients', 'bp_coefficient_errors', 'bp_coefficient_correlations', 'rp_coefficients',
-                         'rp_coefficient_errors', 'rp_coefficient_correlations']
-        converters = dict([(column, lambda x: str_to_array(x)) for column in array_columns])
-        df = pd.read_csv(missing_bp_csv_file, converters=converters)
-        output_df, sampling = convert(df, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
+        inputs = [with_missing_bp_csv_file, missing_bp_csv_file]
+        solutions = [with_missing_solution_df, missing_solution_df]
+        for _input, solution in zip(inputs, solutions):
+            df = pd.read_csv(_input, converters=_converters)
+            output_df, sampling = convert(df, save_file=False)
+            pdt.assert_frame_equal(output_df, solution, rtol=_rtol, atol=_atol)
+            npt.assert_array_equal(sampling, with_missing_solution_sampling)
 
     def test_missing_bp_parsed_dataframe(self):
         # Test completely parsed (arrays + matrices) dataframe
-        df, _ = InputReader(with_missing_bp_csv_file, convert).read()
-        output_df, sampling = convert(df, save_file=False)
-        pdt.assert_frame_equal(output_df, with_missing_solution_df, rtol=_rtol, atol=_atol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
-
-    def test_missing_bp_parsed_dataframe_isolated(self):
-        # Test completely parsed (arrays + matrices) dataframe
-        df, _ = InputReader(missing_bp_csv_file, convert).read()
-        output_df, sampling = convert(df, save_file=False)
-        pdt.assert_frame_equal(output_df, missing_solution_df, atol=_atol, rtol=_rtol)
-        npt.assert_array_equal(sampling, with_missing_solution_sampling)
+        inputs = [with_missing_bp_csv_file, missing_bp_csv_file]
+        solutions = [with_missing_solution_df, missing_solution_df]
+        for _input, solution in zip(inputs, solutions):
+            df, _ = InputReader(_input, convert).read()
+            output_df, sampling = convert(df, save_file=False)
+            pdt.assert_frame_equal(output_df, solution, rtol=_rtol, atol=_atol)
+            npt.assert_array_equal(sampling, with_missing_solution_sampling)

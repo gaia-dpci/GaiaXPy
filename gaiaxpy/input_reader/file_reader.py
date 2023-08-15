cef6a2b526531bc8f587f9c31d3296c1cfbb98d8
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-08-15 12:47:41 +0100

    Add mandatory columns dict and parameter

--- gaiaxpy/input_reader/file_reader.py
+++ gaiaxpy/input_reader/file_reader.py
@@ -6,8 +6,8 @@ def external():
     return ExternalParser()
 
 
-def internal_continuous():
-    return InternalContinuousParser()
+def internal_continuous(mandatory_columns):
+    return InternalContinuousParser(mandatory_columns=mandatory_columns)
 
 
 def raise_error():
@@ -25,6 +25,32 @@ function_parser_dict = {'apply_colour_equation': raise_error,
                         'simulate_continuous': external,
                         'simulate_sampled': external}
 
+# Mandatory columns for each tool (technically, xp_n_relevant_bases are optional depending on "truncation")
+__CAL_MANDATORY_COLS = ['source_id', 'bp_n_parameters', 'bp_standard_deviation', 'bp_coefficients',
+                        'bp_coefficient_errors', 'bp_coefficient_correlations', 'rp_n_parameters',
+                        'rp_standard_deviation', 'rp_coefficients', 'rp_coefficient_errors',
+                        'rp_coefficient_correlations', 'bp_n_relevant_bases', 'rp_n_relevant_bases']
+
+__CON_MANDATORY_COLS = ['source_id', 'bp_basis_function_id', 'bp_n_parameters', 'bp_standard_deviation',
+                        'bp_coefficients', 'bp_coefficient_errors', 'bp_coefficient_correlations',
+                        'rp_basis_function_id', 'rp_n_parameters', 'rp_standard_deviation',
+                        'rp_coefficients', 'rp_coefficient_errors', 'rp_coefficient_correlations',
+                        'bp_n_relevant_bases', 'rp_n_relevant_bases']
+
+__GEN_MANDATORY_COLS = ['source_id', 'bp_n_parameters', 'bp_standard_deviation', 'bp_coefficients',
+                        'bp_coefficient_errors', 'bp_coefficient_correlations', 'rp_n_parameters',
+                        'rp_standard_deviation', 'rp_coefficients', 'rp_coefficient_errors',
+                        'rp_coefficient_correlations']
+
+__INV_MANDATORY_COLS = ['source_id', 'bp_n_parameters', 'bp_standard_deviation', 'bp_coefficient_errors',
+                         'bp_coefficient_correlations', 'rp_n_parameters', 'rp_standard_deviation',
+                         'rp_coefficient_errors', 'rp_coefficient_correlations']
+
+MANDATORY_COLS = {'_calibrate': __CAL_MANDATORY_COLS, 'calibrate': __CAL_MANDATORY_COLS,
+                  'convert': __CON_MANDATORY_COLS, 'generate': __GEN_MANDATORY_COLS,
+                  'get_inverse_covariance_matrix': __INV_MANDATORY_COLS,
+                  'get_inverse_square_root_covariance_matrix': __INV_MANDATORY_COLS}
+
 
 class FileReader(object):
 
@@ -33,4 +59,5 @@ class FileReader(object):
         self.disable_info = disable_info
 
     def select(self):
-        return function_parser_dict[self.function]()
+        mandatory_columns = MANDATORY_COLS[self.function]
+        return function_parser_dict[self.function](mandatory_columns)

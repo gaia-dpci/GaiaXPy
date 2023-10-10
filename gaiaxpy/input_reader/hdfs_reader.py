32bb072f29967e879f835bf1d71080fca884fba8
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-10-10 18:16:24 +0100

    Remove old read method

--- gaiaxpy/input_reader/hdfs_reader.py
+++ gaiaxpy/input_reader/hdfs_reader.py
@@ -1,21 +1,19 @@
 import subprocess
 
-import pandas as pd
 from hdfs import InsecureClient
 from hdfs.ext.avro import AvroReader
 
-from gaiaxpy.core.generic_functions import array_to_symmetric_matrix, _warning
-from gaiaxpy.core.satellite import BANDS
-from gaiaxpy.file_parser.cast import _cast
-from gaiaxpy.file_parser.parse_internal_continuous import InternalContinuousParser
+from gaiaxpy.core.generic_functions import _warning
 from gaiaxpy.input_reader.file_reader import FileReader
-from gaiaxpy.spectrum.utils import get_covariance_matrix
 
 
 class HDFSReader(FileReader):
 
     def __init__(self, file_parser_selector, file, additional_columns=None, selector=None, disable_info=False):
-        self.address, self.file, self.port = self.split_cluster_path(file)
+        self.address, self.file_path, self.port = self.split_cluster_path(file)
+        client = InsecureClient(f'{self.address}:{self.port}')
+        with AvroReader(client, self.file_path) as reader:
+                self.file = reader
         super().__init__(file_parser_selector, self.file, additional_columns, selector, disable_info)
 
     def split_cluster_path(self, file_path, expected_protocol='http', p_sep='://'):
@@ -53,29 +51,3 @@ class HDFSReader(FileReader):
             address, port = process_address_and_port(file_path, protocol_split_index)
             file = file_path[protocol_split_index:]
             return address, file, port
-
-    def read(self):
-        client = InsecureClient(f'{self.address}:{self.port}')
-
-        def __get_records(_avro_file, _additional_columns, _selector):
-            def __yield_records(_avro_file):
-                with AvroReader(client, _avro_file) as reader:
-                    _records = reader.content
-                    for _record in _records:
-                        yield _record
-
-            records = __yield_records(self.file)
-            records = records if _selector is None else filter(_selector, records)
-            for record in records:
-                yield InternalContinuousParser.__process_avro_record(record, _additional_columns)
-
-        df = pd.DataFrame(__get_records(self.file, self.additional_columns, self.selector))
-        # Pairs of the form (matrix_size (N), values_to_put_in_matrix)
-        to_matrix_columns = [('bp_n_parameters', 'bp_coefficient_covariances'),
-                             ('rp_n_parameters', 'rp_coefficient_covariances')]
-        for size_column, values_column in to_matrix_columns:
-            df[values_column] = df.apply(lambda row: array_to_symmetric_matrix(row[values_column], row[size_column]),
-                                         axis=1)
-        for band in BANDS:
-            df[f'{band}_covariance_matrix'] = df.apply(get_covariance_matrix, axis=1, args=(band,))
-        return _cast(df), self.file_extension

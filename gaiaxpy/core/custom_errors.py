d4df4140ec36a0ddcf83e87215d5a453a34c4fb7
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-11-01 17:22:48 +0000

    Add no bands error

--- gaiaxpy/core/custom_errors.py
+++ gaiaxpy/core/custom_errors.py
@@ -1,3 +1,7 @@
+class ExtensionNotImplementedError(Exception):
+    def __init__(self, extension):
+        super().__init__(f"This functionality is not implemented for the file extension '{extension}'")
+
 
 class InvalidBandError(Exception):
     def __init__(self, band):
@@ -5,12 +9,11 @@ class InvalidBandError(Exception):
         super().__init__(self.message)
 
 
+class NoBandsAvailableError(Exception):
+    def __init__(self):
+        super().__init__('No bands present in the input data. At least one band must be available.')
+
+
 class SelectorNotImplementedError(Exception):
     def __init__(self, reader='This'):
         super().__init__(f'{reader} reader does not implement the selector functionality.')
-
-
-class ExtensionNotImplementedError(Exception):
-
-    def __init__(self, extension):
-        super().__init__(f"This functionality is not implemented for the file extension '{extension}'")

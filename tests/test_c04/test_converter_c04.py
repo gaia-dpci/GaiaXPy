df7614999796179251afdabb49df184dbdc6fd9d
Daniela Ruz <dr540@ast.cam.ac.uk>
2023-11-10 12:15:42 +0000

    Add anonymised simple spline test

---
+++ tests/test_c04/test_converter_c04.py
@@ -0,0 +1,20 @@
+from os.path import join
+
+import numpy as np
+import pandas as pd
+import pandas.testing as pdt
+
+from gaiaxpy.config.paths import spline_bases_file
+from gaiaxpy.converter.converter import _convert
+from gaiaxpy.core.generic_functions import str_to_array
+from tests.files.paths import files_path
+
+
+def test_missing_band_source():
+    test_files_path = join(files_path, 'xp_continuous_c04')
+    input_file = join(test_files_path, 'regular_single_source.avro')
+    output_file = join(test_files_path, 'regular_single_source_converter_c04.csv')
+    solution_df = pd.read_csv(output_file, float_precision='high', converters={'flux': lambda x: str_to_array(x),
+                                                                               'flux_error': lambda x: str_to_array(x)})
+    output_df, _ = _convert(input_file, save_file=False, config_file=spline_bases_file)
+    pdt.assert_frame_equal(output_df, solution_df)

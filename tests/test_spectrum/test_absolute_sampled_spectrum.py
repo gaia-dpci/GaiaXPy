f4c72b0a1d85c8b223cf482e72f3d1bf7bf2f3de
Daniela Ruz <dr540@ast.cam.ac.uk>
2024-02-21 12:16:32 +0000

    Refactor spectrum tests

--- tests/test_spectrum/test_absolute_sampled_spectrum.py
+++ tests/test_spectrum/test_absolute_sampled_spectrum.py
@@ -1,4 +1,3 @@
-import unittest
 from configparser import ConfigParser
 
 from gaiaxpy.config.paths import config_ini_file
@@ -11,39 +10,34 @@ from gaiaxpy.spectrum.utils import _correlation_to_covariance_dr3int5
 from gaiaxpy.spectrum.xp_continuous_spectrum import XpContinuousSpectrum
 from tests.files.paths import mean_spectrum_csv_file
 
-configparser = ConfigParser()
-configparser.read(config_ini_file)
 
-label = 'calibrator'
+def test_init_truncation():
+    configparser = ConfigParser()
+    configparser.read(config_ini_file)
 
-xp_sampling_grid, xp_merge = load_xpmerge_from_xml()
-xp_design_matrices = load_xpsampling_from_xml()
+    xp_sampling_grid, xp_merge = load_xpmerge_from_xml()
+    xp_design_matrices = load_xpsampling_from_xml()
 
-parser = InternalContinuousParser()
-parsed_correlation, _ = parser.parse_file(mean_spectrum_csv_file)
+    parser = InternalContinuousParser()
+    parsed_correlation, _ = parser.parse_file(mean_spectrum_csv_file)
 
-# Create sampled basis functions
-sampled_basis_func = {band: SampledBasisFunctions.from_design_matrix(xp_sampling_grid, xp_design_matrices[band])
-                      for band in BANDS}
-
-
-class TestAbsoluteSampledSpectrum(unittest.TestCase):
-
-    def test_init_truncation(self):
-        truncations = [{}, {'bp': 50, 'rp': 40}]
-        for truncation in truncations:
-            parsed_correlation_dict = parsed_correlation.to_dict('records')
-            for row in parsed_correlation_dict:
-                source_id = row['source_id']
-                cont_dict = {}
-                # Split both bands
-                for _band in BANDS:
-                    covariance_matrix = _correlation_to_covariance_dr3int5(row[f'{_band}_coefficient_correlations'],
-                                                                           row[f'{_band}_coefficient_errors'],
-                                                                           row[f'{_band}_standard_deviation'])
-                    continuous_object = XpContinuousSpectrum(source_id, _band, row[f'{_band}_coefficients'],
-                                                             covariance_matrix, row[f'{_band}_standard_deviation'])
-                    cont_dict[_band] = continuous_object
-                spectrum = AbsoluteSampledSpectrum(source_id, cont_dict, sampled_basis_func, xp_merge,
-                                                   truncation=truncation)
-                self.assertIsInstance(spectrum, AbsoluteSampledSpectrum)
+    # Create sampled basis functions
+    sampled_basis_func = {band: SampledBasisFunctions.from_design_matrix(xp_sampling_grid, xp_design_matrices[band])
+                          for band in BANDS}
+    truncations = [{}, {'bp': 50, 'rp': 40}]
+    for truncation in truncations:
+        parsed_correlation_dict = parsed_correlation.to_dict('records')
+        for row in parsed_correlation_dict:
+            source_id = row['source_id']
+            cont_dict = {}
+            # Split both bands
+            for _band in BANDS:
+                covariance_matrix = _correlation_to_covariance_dr3int5(row[f'{_band}_coefficient_correlations'],
+                                                                       row[f'{_band}_coefficient_errors'],
+                                                                       row[f'{_band}_standard_deviation'])
+                continuous_object = XpContinuousSpectrum(source_id, _band, row[f'{_band}_coefficients'],
+                                                         covariance_matrix, row[f'{_band}_standard_deviation'])
+                cont_dict[_band] = continuous_object
+            spectrum = AbsoluteSampledSpectrum(source_id, cont_dict, sampled_basis_func, xp_merge,
+                                               truncation=truncation)
+            assert isinstance(spectrum, AbsoluteSampledSpectrum)
--- tests/test_spectrum/test_generic_spectrum.py
+++ tests/test_spectrum/test_generic_spectrum.py
@@ -1,12 +1,8 @@
-import unittest
-
 from gaiaxpy.spectrum.generic_spectrum import Spectrum
 
 
-class TestSpectrum(unittest.TestCase):
-
-    def test_spectrum_init(self):
-        source_id = 5626132279854336
-        spectrum = Spectrum(source_id)
-        self.assertIsInstance(spectrum, Spectrum)
-        self.assertEqual(spectrum.get_source_id(), source_id)
+def test_spectrum_init():
+    source_id = 5626132279854336
+    spectrum = Spectrum(source_id)
+    assert isinstance(spectrum, Spectrum)
+    assert spectrum.get_source_id() == source_id

2d69277db6d271f865a64a2773d343a3492d38f8
Daniela Ruz <dr540@ast.cam.ac.uk>
2024-02-22 15:08:19 +0000

    Update tolerance (which may vary depending on the OS)

--- tests/test_generator/test_generator.py
+++ tests/test_generator/test_generator.py
@@ -8,8 +8,7 @@ from gaiaxpy import generate, PhotometricSystem
 from gaiaxpy.file_parser.cast import _cast
 from tests.files.paths import missing_bp_csv_file, mean_spectrum_fits_file, gen_missing_band_sol_path
 
-_ertol, _eatol = 1e-23, 1e-23
-_rtol, _atol = 1e-14, 1e-14
+_rtol, _atol = 1e-23, 1e-23
 
 
 @pytest.fixture(scope='module')
@@ -28,12 +27,8 @@ def systems_list():
 
 def test_missing_band_csv(systems_list):
     generated_photometry = generate(missing_bp_csv_file, photometric_system=systems_list, save_file=False)
-    # Load solution
     solution_df = _cast(pd.read_csv(gen_missing_band_sol_path, float_precision='round_trip'))
-    error_columns = [column for column in solution_df.columns if 'error' in column]
-    other_columns = [column for column in solution_df.columns if 'error' not in column]
-    pdt.assert_frame_equal(generated_photometry[error_columns], solution_df[error_columns], rtol=_ertol, atol=_eatol)
-    pdt.assert_frame_equal(generated_photometry[other_columns], solution_df[other_columns], rtol=_rtol, atol=_atol)
+    pdt.assert_frame_equal(generated_photometry, solution_df, rtol=_rtol, atol=_atol)
 
 
 def test_duplicate_columns(systems_list):

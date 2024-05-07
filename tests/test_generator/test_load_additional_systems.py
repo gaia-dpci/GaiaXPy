1eb420b149cde60746d9bf61c10234f7500ac617
Daniela Ruz <dr540@ast.cam.ac.uk>
2024-05-07 16:07:20 +0100

    Pass PhotometricSystem as fixture

--- tests/test_generator/test_load_additional_systems.py
+++ tests/test_generator/test_load_additional_systems.py
@@ -1,18 +1,22 @@
+import pytest
+
 from gaiaxpy import load_additional_systems, generate, remove_additional_systems
 from tests.files.paths import with_missing_bp_csv_file
 from tests.test_generator.generator_paths import additional_filters_dir
 
 
-def setup():
+@pytest.fixture
+def __ps():
     PhotometricSystem = remove_additional_systems()
+    yield PhotometricSystem
 
 
-def test_load_additional_with_err_corr():
+def test_load_additional_with_err_corr(__ps):
     expected_columns = ['source_id', 'GaiaDr3Vega_mag_G', 'GaiaDr3Vega_mag_BP', 'GaiaDr3Vega_mag_RP',
                         'GaiaDr3Vega_flux_G', 'GaiaDr3Vega_flux_BP', 'GaiaDr3Vega_flux_RP', 'GaiaDr3Vega_flux_error_G',
                         'GaiaDr3Vega_flux_error_BP', 'GaiaDr3Vega_flux_error_RP']
-    PhotometricSystem = load_additional_systems(additional_filters_dir)
-    phot_systems = [PhotometricSystem.Gaia_DR3_Vega]
+    __ps = load_additional_systems(additional_filters_dir)
+    phot_systems = [__ps.Gaia_DR3_Vega]
     photometry = generate(with_missing_bp_csv_file, photometric_system=phot_systems, error_correction=True,
                           save_file=False)
     assert len(photometry) == 3
--- tests/test_generator/test_photometric_system.py
+++ tests/test_generator/test_photometric_system.py
@@ -4,14 +4,17 @@ from numpy import ndarray
 
 from gaiaxpy import generate
 from gaiaxpy.core.generic_functions import _get_built_in_systems
-from gaiaxpy.generator.photometric_system import PhotometricSystem, load_additional_systems, remove_additional_systems
+from gaiaxpy.generator.photometric_system import load_additional_systems, remove_additional_systems
 from tests.files.paths import with_missing_bp_ecsv_file
 from tests.test_generator.generator_paths import additional_filters_dir, additional_filters_dup_dir
 from tests.test_generator.test_internal_photometric_system import phot_systems_specs
 
 
-def setup():
+@pytest.fixture(scope='module')
+def __ps():
     PhotometricSystem = remove_additional_systems()
+    yield PhotometricSystem
+
 
 def get_system_by_name(lst, name):
     return [item[1] for item in lst if item[0] == name][0]
@@ -23,17 +26,17 @@ def available_systems():
 
 
 @pytest.fixture()
-def phot_systems():
-    phot_system_names = [phot_system.name for phot_system in PhotometricSystem]
-    yield [(name, PhotometricSystem[name]) for name in phot_system_names]
+def phot_systems(__ps):
+    phot_system_names = [phot_system.name for phot_system in __ps]
+    yield [(name, __ps[name]) for name in phot_system_names]
 
 
-def test_system_is_standardised():
+def test_system_is_standardised(__ps):
     """
     Check class assigned to each photometric system. Will raise an error if a system is missing in the solution
     dictionary.
     """
-    all_phot_systems = [PhotometricSystem[s] for s in PhotometricSystem.get_available_systems().split(', ')]
+    all_phot_systems = [__ps[s] for s in __ps.get_available_systems().split(', ')]
     regular_systems = ["DECam", "Els_Custom_W09_S2", "Euclid_VIS", "Gaia_2", "Gaia_DR3_Vega", "Halpha_Custom_AB",
                        "H_Custom", "Hipparcos_Tycho", "HST_ACSWFC", "HST_WFC3UVIS", "HST_WFPC2", "IPHAS", "JKC",
                        "JPAS", "JPLUS", "JWST_NIRCAM", "LSST", "PanSTARRS1", "Pristine", "SDSS", "Sky_Mapper",
@@ -43,12 +46,13 @@ def test_system_is_standardised():
     standardised_dict = {s: "StandardisedPhotometricSystem" for s in standardised_systems}
     solution_dict = {**regular_dict, **standardised_dict}
     for system in all_phot_systems:
-        assert system.value.__class__.__name__ == solution_dict[system.name], f'Test has failed for system {system.name}.'
+        assert system.value.__class__.__name__ == solution_dict[
+            system.name], f'Test has failed for system {system.name}.'
 
 
-def test_init(phot_systems):
+def test_init(phot_systems, __ps):
     for name, phot_system in phot_systems:
-        assert isinstance(phot_system, PhotometricSystem)
+        assert isinstance(phot_system, __ps)
 
 
 def test_get_system_label(phot_systems, available_systems):
@@ -81,33 +85,33 @@ def test_get_set_zero_points(phot_systems, available_systems):
         npt.assert_array_equal(system_zero_points, test_zero_points)
 
 
-def test_user_interaction():
-    global PhotometricSystem
-    phot_system_list = [s for s in PhotometricSystem.get_available_systems().split(', ')]
+def test_user_interaction(__ps):
+    phot_system_list = [s for s in __ps.get_available_systems().split(', ')]
     built_in_systems = _get_built_in_systems()
     assert set(phot_system_list) == set(built_in_systems)
-    PhotometricSystem = load_additional_systems(additional_filters_dir)
-    new_phot_systems = [s for s in PhotometricSystem.get_available_systems().split(', ')]
+    __ps = load_additional_systems(additional_filters_dir)
+    new_phot_systems = [s for s in __ps.get_available_systems().split(', ')]
     assert len(phot_system_list) + 5 == len(new_phot_systems)
-    ps = [PhotometricSystem[s] for s in ['Pristine', 'SDSS', 'PanSTARRS1_Std', 'USER_Panstarrs1Std', 'USER_Sdss',
-                                           'USER_Pristine']]
+    ps = [__ps[s] for s in ['Pristine', 'SDSS', 'PanSTARRS1_Std', 'USER_Panstarrs1Std', 'USER_Sdss',
+                            'USER_Pristine']]
     output = generate(with_missing_bp_ecsv_file, photometric_system=ps, save_file=False)
     built_in_columns = [c for c in output.columns if not c.startswith('USER')]
     built_in_columns.remove('source_id')
     for column in built_in_columns:
         npt.assert_array_equal(output[column].values, output[f'USER_{column}'].values)
-    PhotometricSystem = remove_additional_systems()
-    phot_system_list = [s for s in PhotometricSystem.get_available_systems().split(', ')]
+    __ps = remove_additional_systems()
+    phot_system_list = [s for s in __ps.get_available_systems().split(', ')]
     assert set(phot_system_list) == set(built_in_systems)
 
 
-def test_additional_systems_names():
-    PhotometricSystem = load_additional_systems(additional_filters_dir)
-    ps = [PhotometricSystem[s].get_system_name() for s in PhotometricSystem.get_available_systems().split(', ') if
+def test_additional_systems_names(__ps):
+    __ps = load_additional_systems(additional_filters_dir)
+    ps = [__ps[s].get_system_name() for s in __ps.get_available_systems().split(', ') if
           s.startswith('USER')]
     assert ps == ['USER_Panstarrs1Std', 'USER_Sdss', 'USER_Pristine', 'USER_ASubstring_AndMore', 'USER_ASubstring']
 
 
-def test_duplicate_names_in_dir():
+def test_duplicate_names_in_dir(__ps):
+    __ps = remove_additional_systems()
     with pytest.raises(ValueError):
         load_additional_systems(additional_filters_dup_dir)
